e6808642e5023d3032f47dd563b9c139dd1dd41a
tama9884649 <sashoyaruki0@docomo.ne.jp>
2017-06-26 18:13:02 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,18 +2,22 @@ for(var i = 1; i <= 100; i++)
 {
   var li = document.createElement('li');
  
-  li.textContent='';
+ 
   
 
    if(i%3==0)
   {
-   li.textContent+='Fizz'; 
-  }
-   else if(i%5==0)
+   li.textContent='Fizz'; 
+  if(i%5==0)
   {
-   li.textContent+='Buzz'; 
+   li.textContent='FizzBuzz'; 
+  }
   }else{
-      li.textContent =i;
+     if(i%5==0)
+  {
+   li.textContent='Buzz'; 
+  }
+    li.textContent =i;
   }
   document.getElementById('list').appendChild(li);
 }
