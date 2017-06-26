cb9d0cf7d12a1822b9ec83453659532e7eb3a2eb
tama9884649 <sashoyaruki0@docomo.ne.jp>
2017-06-26 17:43:30 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,15 +1,12 @@
-for(var i = 1; i < 10; i++){
+for(var i = 1; i < 100; i++){
 var li = document.createElement('li');
 li.textContent = i + '回目';
 document.getElementById('list').appendChild(li);
 }
 
-var HP=100;
-while(0 < HP){
-var li = document.createElement('li');
-var damage = Math.floor(Math.random() * 100);
-HP -= damage;
-HP = (hp < 0) ? 0: HP;
-li.textContent = damage + 'のダメージを受けた。残りのHP：' + HP;
-document.getElementById('while').appendChild(li);
+for(var i = 1; i < 100; i++){
+  var li = document.createElement('li');
+  var price = Math.floor(Math.random() * 1000);
+  li.textContent =i;
+  document.getElementById('price').appendChild(li);
 }
