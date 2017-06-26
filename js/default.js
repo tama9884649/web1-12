7664568363ce31469840037787db9c2b102ea2ca
tama9884649 <sashoyaruki0@docomo.ne.jp>
2017-06-26 17:48:19 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,7 +6,6 @@ document.getElementById('list').appendChild(li);
 
 for(var i = 1; i < 100; i++){
   var li = document.createElement('li');
-  var price = Math.floor(Math.random() * 1000);
   li.textContent =i;
   document.getElementById('price').appendChild(li);
 }
