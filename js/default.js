c1e10cec711077be50a43264cc37edf81c75a140
tama9884649 <sashoyaruki0@docomo.ne.jp>
2017-06-26 18:07:38 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,9 +8,7 @@ for(var i = 1; i <= 100; i++)
   if(i%15==0)
   {
    li.textContent='FizzBuzz'; 
-  }else if(1%6==0){
-    
-    li.textContent='Im  CRAZY BOY!!!!!!!!'
+  }
    else if(i%3==0)
   {
    li.textContent='Fizz'; 
